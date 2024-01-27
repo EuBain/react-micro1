f16df345bdff2c0b20bfff7b6e56344cdcea26db
EuBain <tayrsi@outlook.com>
2024-01-27 13:48:09 +0800

    子应用props问题

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -12,9 +12,9 @@ const MyLayout = () => {
   } = theme.useToken();
 
   useBusOnChangePath();
-  console.log(window.$wujie?.props?.addNavList);
+  // console.log(window.$wujie?.props?.addNavList);
   useEffect(() => {
-    // console.log(window.$wujie?.props?.addNavList);
+    console.log(window.$wujie?.props?.addNavList);
     console.log(window.__POWERED_BY_WUJIE__);
     if (window.__POWERED_BY_WUJIE__ && window.$wujie?.props?.addNavList) {
       window.$wujie?.props?.addNavList?.(routeModel);
